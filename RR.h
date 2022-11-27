a4e3442d30fb2db3a33ad1871109cde9a8b076fe
Fauzan Azhar <fauzan_azhar786@outlook.com>
2022-11-27 13:07:12 +0500

    Made a Round Robin Scheduling Algorithm analogy.

---
+++ RR.h
@@ -0,0 +1,38 @@
+//
+// Created by Fauzan Azhar on 11/27/2022.
+//
+
+#ifndef CRR_RR_H
+#define CRR_RR_H
+typedef struct Process{
+    int processID;
+    int ArrivalTime;
+    int BurstTime;
+    int RemainingBurstTime;
+    int CompletionTime;
+} Process;
+
+typedef struct Node{
+    Process *data;
+    struct Node *next;
+} Node;
+
+typedef struct LinkedList{
+    Node *head;
+    Node *tail;
+    int size;
+} LinkedList;
+
+void print_process(Process *process);
+
+void enqueue(LinkedList *queue, void *data);
+
+Node *dequeue(LinkedList *queue);
+
+void merge(Process *arr[], int start, int mid, int end);
+
+void sort_by_AT(Process *arr[], int start, int end);
+
+void take_input(Process *arr[], int size);
+
+#endif //CRR_RR_H
---
+++ main.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "RR.h"
+
+int main() {
+    int size;
+    printf("Enter the number of processes: ");
+    scanf(" %d", &size);
+    Process *arr[size];
+    take_input(arr, size);
+    int QuantumTime = 1, Time, index = 0;
+    Time = arr[index]->ArrivalTime;
+    LinkedList *queue = (LinkedList *) malloc(sizeof(LinkedList));
+    queue->head = (Node *) malloc(sizeof(Node));
+    queue->tail = queue->head;
+    queue->size = 0;
+    while ((index < size) && (arr[index]->ArrivalTime <= Time))
+        enqueue(queue, arr[index++]);
+    Process *currentProcess;
+    int RBT;
+    while (queue->head != NULL){
+
+        currentProcess = dequeue(queue)->data;
+        RBT = currentProcess->RemainingBurstTime;
+
+        if (RBT >= QuantumTime) {
+            currentProcess->RemainingBurstTime = RBT - QuantumTime;
+            Time += QuantumTime;
+        } else{
+            Time += RBT;
+            currentProcess->RemainingBurstTime = 0;
+        }
+
+        while ((index < size) && (arr[index]->ArrivalTime <= Time))
+            enqueue(queue, arr[index++]);
+
+        if (currentProcess->RemainingBurstTime == 0)
+            currentProcess->CompletionTime = Time;
+        else
+            enqueue(queue, currentProcess);
+
+        print_process(currentProcess); // Comment This Line (For Debugging)
+    }
+
+    for (int i = 0; i < size; ++i)
+        print_process(arr[i]);
+
+    return 0;
+}
+
+//Take Input of each process and sort them by Arrival Time
+void take_input(Process *arr[], int size){
+    printf("Enter the following for each process, seperated by ',', when asked.\n");
+    printf("Process ID,Arrival Time,Burst Time\n");
+    int ProcessID, ArrivalTime, BurstTime;
+    for (int i = 0; i < size; ++i) {
+        printf("Enter Process # %d Details:\n", i + 1);
+        scanf(" %d,%d,%d", &ProcessID, &ArrivalTime, &BurstTime);
+        arr[i] = (Process *) malloc(sizeof(Process));
+        arr[i]->processID = ProcessID;
+        arr[i]->ArrivalTime = ArrivalTime;
+        arr[i]->BurstTime = BurstTime;
+        arr[i]->RemainingBurstTime = BurstTime;
+    }
+    sort_by_AT(arr, 0, size - 1);
+}
+
+//Merge Sort the process By Arrival Time
+void sort_by_AT(Process *arr[], int start, int end) {
+    if (start < end){
+        int mid = start + (end - start)/2;
+        sort_by_AT(arr, start, mid);
+        sort_by_AT(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+}
+
+void merge(Process *arr[], int start, int mid, int end){
+    int size1 = mid - start + 1;
+    int size2 = end - mid;
+
+    Process *arrL[size1];
+    Process *arrR[size2];
+
+    for (int i = 0; i < size1; ++i)
+        arrL[i] = arr[start + i];
+
+    for (int j = 0; j < size2; ++j)
+        arrR[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k;
+    for (k = 0; i < size1 && j < size2 ; ++k) {
+        if (arrL[i]->ArrivalTime < arrR[j]->ArrivalTime)
+            arr[k] = arrL[i++];
+        else
+            arr[k] = arrR[j++];
+    }
+
+    while (i < size1)
+        arr[k++] = arrL[i++];
+    while (j < size2)
+        arr[k++] = arrR[j++];
+}
+
+void print_process(Process *process){
+    printf("--------------------Process--------------------\n");
+    printf("     Process ID = %d\n", process->processID);
+    printf("     Process Arrival Time = %d\n", process->ArrivalTime);
+    printf("     Process Burst Time = %d\n", process->BurstTime);
+    printf("     Process Remaining Burst Time = %d\n", process->RemainingBurstTime);
+    printf("     Process Completion Time = %d\n", process->CompletionTime);
+    printf("--------------------Process--------------------\n");
+}
+
+void enqueue(LinkedList *queue, void *data) {
+    if (queue->size == 0){
+        queue->head = (Node *) malloc(sizeof(Node));
+        queue->head->data = (Process *) data;
+        queue->head->next = NULL;
+        queue->tail = queue->head;
+    } else if (queue->head == queue->tail){
+        queue->head->next = (Node *) malloc(sizeof(Node));
+        queue->head->next->data = (Process *) data;
+        queue->head->next->next = NULL;
+        queue->tail = queue->head->next;
+    } else{
+        queue->tail->next = (Node *) malloc(sizeof(Node));
+        queue->tail = queue->tail->next;
+        queue->tail->data = (Process *) data;
+        queue->tail->next = NULL;
+    }
+    queue->size++;
+}
+
+Node *dequeue(LinkedList *queue){
+    Node *result = queue->head;
+    if (queue->head == queue->tail)
+        queue->tail = queue->head->next;
+    queue->head = queue->head->next;
+    queue->size--;
+    return result;
+}
